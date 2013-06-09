62a9819547201fbe077112d8145bd358610ce5ab
Richard Morgan <morganric@gmail.com>
2013-06-09 15:09:57 +0100

    js to fade out messages

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ match 'lists/:list_id/items/:id/complete' => 'items#complete', :as => :complete_
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
+  root :to => "lists#index"
   devise_for :users
   resources :users
 end
