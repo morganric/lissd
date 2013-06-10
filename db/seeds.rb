ca65705282223fecd597eeef29ce94a4fceaf8df
Richard Morgan <morganric@gmail.com>
2013-06-10 01:17:36 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,10 @@
 # See http://railsapps.github.io/rails-environment-variables.html
 puts 'ROLES'
 YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+  Role.find_or_create_by_name ({ :name => role }, :without_protection => true)
   puts 'role: ' << role
 end
+debugger
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
